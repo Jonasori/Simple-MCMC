b1b178b1f99fa3b8135ea5ce3abdd14e676de888
Jonas Powell <jonasori@gmail.com>
2018-02-09 17:35:49 -0500

    "Working through it."

--- fit_data.py
+++ fit_data.py
@@ -5,6 +5,7 @@ Develop a simple MCMC code to fit to data generated in utils.py
 """
 
 import numpy as np
+import numpy.random as r
 import matplotlib.pyplot as plt
 
 import utils
@@ -33,10 +34,71 @@ sig_a, sig_b = 3, 3
 proposed_new_step_a = np.random.normal(loc=a_init, scale=sig_a)
 proposed_new_step_b = np.random.normal(loc=b_init, scale=sig_b)
 
-chisq_a = utils.chi2(utils.parabola(
+
+chisq_a_old = utils.chi2(utils.parabola(
                         xs,
                         proposed_new_step_a,
                         b_init),
                         ds)
 
-chisq_b = utils.chi2([proposed_new_step_b], [b_init])
+chisq_a_new = utils.chi2(utils.parabola(
+                        xs,
+                        proposed_new_step_a,
+                        b_init),
+                        ds)
+
+
+if chisq_a_new < chisq_a_old:
+    #accept
+    blah = 1
+
+else:
+    accept_metric_a = np.exp(-chisq_a)
+    random_test_range = np.random.random()
+    if accept_metric_a < random_test_range:
+        # reject
+    else:
+        # accept
+
+nsteps = 100
+position = [[1,1]]
+for i in range(nsteps):
+
+    # Randomly choose which parameter to move
+    # Use this as an index for the positions list
+    p = r.randint(2)
+    p_other = 1 if p==0 else 0
+
+    # Propose a new step
+    new_pos = np.random.normal(loc=position[-1][p], scale=sig[p])
+
+
+
+
+    # Calcualte Chi-Squared for new step, store it
+
+    chisq_a_old = utils.chi2(
+                    utils.parabola(xs, new_pos, pos[-1][p_other]), ds)
+
+    # If chi2_new < chi2_old, accept.
+    # Else, generate a random number in [0,1]
+
+    # If exp(-chi2_new/2) < random, reject
+    # Else, accept
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# The End
