582143b72aca822d4b4173a6d1838e34bd4d37dc
Jonasori <jonasori@gmail.com>
2018-02-19 22:51:10 -0500

    Heavily refactored. Put everything in functions, cleaned things up a bit

--- fit_data.py
+++ fit_data.py
@@ -5,96 +5,136 @@ Develop a simple MCMC code to fit to data generated in utils.py
 """
 
 import numpy as np
-import numpy.random as r
 import matplotlib.pyplot as plt
-import utils
+from utils import parabola, generate_fake_data, chi2
+from utils import truncated_random_normal
 
 # Choose a starting point
-a_init = 10
-b_init = 5
-
-# Add some noise
-sigma = 100
-
+true_a = 10.1
+true_b = 5.1
+true_sigma = 100
 # Generate some data
 xs = np.arange(-20, 20)
-ds = utils.generate_fake_data(xs, a_init, b_init, sigma)
+ys = generate_fake_data(xs, true_a, true_b, true_sigma)
 
 # Specify priors
-priors_a = range
-priors_b = [-20, 20]
+priors_a = [-50, 50]
+priors_b = [-50, 50]
 
 
-# Start the actual MCMC
+def mcmc(xs, ys, priors_a, priors_b, sigma=1, nsteps=1000):
+    # nsteps: how many steps the walkers should take
+    # xs, ys: input data (lists/numpy arrays)
+    # priors_a, priors_b: bounds on where the walkers can go
+    # sigma: noise level
 
-nsteps = 10000
-# The a and b values [a, b]
-a_vals = np.zeros(nsteps)
-b_vals = np.zeros(nsteps)
-chisqs = np.zeros(nsteps)
+    # Initialize the arrays
+    a_vals = np.zeros(nsteps)
+    b_vals = np.zeros(nsteps)
+    chisqs = np.zeros(nsteps)
 
-# Give a starting point and calculate that initial chi2
-a_vals[0], b_vals[0] = a_init, b_init
-first_model = utils.parabola(xs, a_init, b_init)
-chisqs[0] = utils.chi2(ds, first_model, sigma)
+    # Give a starting point and calculate that initial chi2
+    initial_a = np.random.uniform(priors_a[0], priors_a[1])
+    initial_b = np.random.uniform(priors_b[0], priors_b[1])
+    a_vals[0], b_vals[0] = initial_a, initial_b
 
+    first_model = parabola(xs, initial_a, initial_b)
+    chisqs[0] = chi2(ys, first_model, sigma)
 
-for i in range(1, nsteps):
+    # Start the loop!
+    i = 1
+    while i < nsteps:
+        print i
+        # Propose a new step
+        choice = np.random.choice(['a', 'b'], 1)
+        print choice
+        if choice == 'a':
+            # a_new = np.random.normal(loc=a_vals[i-1], scale=sigma)
+            a_new = truncated_random_normal(a_vals[i-1],
+                                            sigma, priors_a[0],
+                                            priors_a[1])[0]
 
-    # Randomly choose which parameter to move
-    # Propose a new step: choose a dimension to move in
-    choice = r.choice(['a', 'b'], 1)
-    if choice == 'a':
-        a_new = np.random.normal(loc=a_vals[i-1], scale=sigma)
-        new_step = np.array([a_new, b_vals[i-1]])
-    else:
-        b_new = np.random.normal(loc=b_vals[i-1], scale=sigma)
-        new_step = np.array([a_vals[i-1], b_new])
+            new_step = np.array([a_new, b_vals[i-1]])
+        else:
+            # b_new = np.random.normal(loc=b_vals[i-1], scale=sigma)
+            b_new = truncated_random_normal(b_vals[i-1],
+                                            sigma, priors_b[0],
+                                            priors_b[1])[0]
 
-    # Calculate Chi-Squared for new step, store it
-    model_new = utils.parabola(xs, new_step[0], new_step[1])
-    chisq_new = utils.chi2(ds, model_new, sigma)
+            new_step = np.array([a_vals[i-1], b_new])
 
-    # Make sure the new step doesn't violate the priors:
-    if utils.is_valid_step(new_step, priors_a, priors_b):
+        print new_step
+        # Calculate Chi-Squared for that new step
+        model_new = parabola(xs, new_step[0], new_step[1])
+        chisq_new = chi2(ys, model_new, sigma)
+
+        # Make sure the new step doesn't violate the priors
+        # Note that if this fails, the loop starts over the with same i value
+        # if is_valid_step(new_step, priors_a) and is_valid_step(new_step, priors_b):
 
         # If the new one is an improvement, take it.
-        if chisq_new > chisqs[i-1]:
+        if chisq_new < chisqs[i-1]:
             a_vals[i], b_vals[i] = new_step[0], new_step[1]
             chisqs[i] = chisq_new
 
         # Otherwise, generate a random number in [0,1]
         else:
-            r_num = r.random()
+            r_num = np.random.random()
             # delta_chisq = chisq_old - chisq_new from eqn(13) of Ford 2005
             delta_chisq = (chisqs[i-1] - chisq_new)
             alpha = np.exp(delta_chisq/2)
 
-            # If exp(-dChi2/2) < random, reject.
-            # It feels weird that we reject if ex is smaller.
+            # If alpha < random, reject this step.
+            # It feels weird that we reject if alpha is smaller.
             if alpha < r_num:
                 a_vals[i], b_vals[i] = a_vals[i-1], b_vals[i-1]
                 chisqs[i] = chisqs[i-1]
 
-            # Else, accept
+            # If alpha > random, accept the new step.
             else:
                 a_vals[i], b_vals[i] = new_step[0], new_step[1]
                 chisqs[i] = chisq_new
 
+        # Bump the counter
+        i += 1
+
+        print '\n\n'
+
+    # Final outputs
+    # best_fit_index = chisqs.index(min(chisqs))
+    # best_fit_vals = [a_vals[best_fit_index], b_vals[best_fit_index]]
+    final_output = {
+                    'a_vals_visited': a_vals,
+                    'b_vals_visited': b_vals,
+                    'chi2_vals': chisqs,
+                    }
+
+    # 'best_fit_vals': best_fit_vals
+
+    return final_output
+
+
+# A generic plotter
+def plot_whatever(xs, ys):
+    plt.plot(xs, ys, '-og')
+    plt.show(block=False)
+
 
 # Plot it in parameter space
-plt.plot(a_vals, b_vals, '.k', alpha=0.1)
-plt.xlabel('a')
-plt.ylabel('b')
-plt.show(block=False)
+def plot_param_walk(a_vals, b_vals):
+    plt.plot(a_vals, b_vals, '.k', alpha=0.1)
+    plt.xlabel('a')
+    plt.ylabel('b')
+    plt.show(block=False)
+
 
 # Plot the resulting parabola
-"""
-best_fit_index =
-ys = utils.parabola(xs, coeffs[-1][0], coeffs[-1][1])
-plt.plot(xs, ys)
-plt.show(block=False)
-"""
+def plot_best_fit_model(chisqs):
+    best_fit_index = chisqs.index(min(chisqs))
+    ys = parabola(xs, coeffs[-1][0], coeffs[-1][1])
+    plt.plot(xs, ys)
+    plt.show(block=False)
+
 
 
 
--- utils.py
+++ utils.py
@@ -1,17 +1,13 @@
 """
 Basic MCMC Pt. 1: Some Tools.
 
-Generate a noisy parabola
+Generate a noisy parabola, and other tools to be used in fit_data.py
 """
 
 # Packages
 import numpy as np
 import matplotlib.pyplot as plt
-
-
-# Parabola parameters
-a = 10
-b = 30
+from scipy.stats import truncnorm
 
 
 # Make a Parabola from a list of xs
@@ -25,38 +21,18 @@ parabola([1, 2, 3, 4, 5, 6], 1, 0)
 
 
 # Generate some a noisy parabola with parameters a, b, and c
-def generate_fake_data(xs, a, b, noisiness):
+def generate_fake_data(xs, a, b, noise):
     ys_clean = parabola(xs, a, b)
-    # Should probably be able to do this with list ops
-    ys_noisy = [np.random.normal(loc=y, scale=noisiness, size=1) for y in ys_clean]
-    return ys_noisy
+    ys_dirty = [np.random.normal(loc=y, scale=noise, size=1) for y in ys_clean]
+
+    return ys_dirty
 
 
-# Check if a step is within the priors
-def is_valid_step(step, priors_a, priors_b):
-    # Each argument is a tuple
-    if not priors_a[0] < step[0] < priors_a[1]:
-        return False
-    if not priors_b[0] < step[1] < priors_b[1]:
-        return False
-    else:
-        return True
-
-# Testing Stuff
-"""
-xs = np.arange(-10, 10)
-ys = generate_fake_data(xs, a, b, 30.)
-
-print len(xs)
-print len(ys)
-
-plt.plot(xs, ys, '-o')
-plt.show()
-"""
-
-
-ms = np.arange(1, 10)
-ds = [np.random.normal(loc=m) for m in ms]
+def generate_fake_data_2(xs, a, b, noise):
+    dirty_a = np.random.normal(loc=a, scale=noise, size=1)
+    dirty_b = np.random.normal(loc=b, scale=noise, size=1)
+    ys_dirty = parabola(xs, dirty_a, dirty_b)
+    return ys_dirty
 
 
 # Calculate Chi-Squared
@@ -68,15 +44,64 @@ def chi2(data, model, sigma):
     val = np.sum((np.array(data) - np.array(model))**2 / sigma**2)
     return val
 
-chi2([1,2,3,4], [1.1, 2.1, 3.1, 4.1], 0.1)
+
+chi2([1, 2, 3, 4], [1.1, 2.1, 3.1, 4.1], 0.1)
+
+
+# Check if a step is within the priors
+def is_valid_step(step, priors):
+    # Each argument is a tuple
+    step_ok = priors[0] < step[0] and step[0] < priors[1]
+    # Do the logics
+    if step_ok:
+        return True
+    else:
+        return False
+
+
+# Calculate acceptance fraction:
+def calculate_acceptance_fraction():
+    blah = 'blah'
+    return blah
+
+
+# Truncated Normal Distribution
+# Pulled from:
+# stackoverflow -> how-to-get-a-normal-distribution-within-a-range-in-numpy
+def truncated_random_normal(mean, sd, low, upp, N=1):
+    """
+    - To be used instead of np.random.normal and then binding that.
+    - N defaults to 1 to only return one value.
+        - Note that this means its a list, so be sure to drop an [0] on the end
+    To verify that this yields a normal distribution:
+        ys = truncated_random_normal(0, 1, -10, 10, 1000)
+        plt.hist(ys); plt.show()
+    """
+    X = truncnorm((low - mean) / sd, (upp - mean) / sd, loc=mean, scale=sd)
+    return X.rvs(N)
+
+
+
+
+
+# TESTING AREA
+def test_plots(xs, ys):
+    plt.plot(xs, ys, '-o')
+    plt.show()
+
+
+ms = np.arange(1, 10)
+ds = [np.random.normal(loc=m) for m in ms]
+
+
 
 # More testing Stuff
-"""
-c2 = chi2(ms, ds)
-plt.plot(ms, ms, '-og')
-plt.plot(ms, ds, '-or')
-plt.plot(ms, c2, 'ob')
-plt.show()
-"""
+def more_testing_plots():
+    c2 = chi2(ms, ds)
+    plt.plot(ms, ms, '-og')
+    plt.plot(ms, ds, '-or')
+    plt.plot(ms, c2, 'ob')
+    plt.show()
+
 
 # The End
